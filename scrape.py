a2b6aa5ad90d181859df0099b78bce6612c5a9de
Gabriel Kabbe <gabriel.kabbe@mail.de>
2017-12-21 19:41:29 +0100

    Fix scraping loop

--- scrape.py
+++ scrape.py
@@ -10,7 +10,7 @@ logger = daiquiri.getLogger()
 
 
 cp = configparser.ConfigParser()
-cp.read_file("scrape.cfg")
+cp.read("scrape.cfg")
 
 
 URL = cp.get("scrape", "url")
@@ -22,9 +22,8 @@ xpath = '//*[@id="page"]/div/a/img'
 
 
 def get_links(driver):
-    wrappers = driver.find_elements_by_xpath('//*[@class="element"]')
-    links = [elem.find_elements_by_tag_name("a")[1].get_attribute("href")
-             for elem in wrappers]
+    all_links = driver.find_elements_by_xpath('//*/a/@href')
+    links = [link for link in all_links if "/read/" in link]
     return links
 
 
@@ -32,12 +31,16 @@ def open_comic(driver, link):
     driver.get(link)
     pic_links = []
     while True:
-        pic = driver.find_element_by_xpath(xpath)
+        try:
+            pic = driver.find_element_by_xpath(xpath)
+        except selenium.common.exceptions.NoSuchElementException:
+            logger.info("No more pictures found")
+            break
         pic_link = pic.get_attribute("src")
-        print(f"Found pic {pic_link}")
+        logger.info(f"Found pic {pic_link}")
         pic_links.append(pic_link)
         pic.click()
-        time.sleep(0.5)
+        time.sleep(0.2)
     return pic_links
 
 
